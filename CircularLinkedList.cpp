5a6d1d342c05fda0a7b8313dd7411687d03a4efb
Yabets solomon <jabezsolomonz@gmail.com>
2024-11-13 22:13:30 +0300

    single Linked List and circular linked list added to this repo

---
+++ CircularLinkedList.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+struct LinkedList
+{
+    LinkedList *next;
+    LinkedList *prev;
+    int data;   
+};
+void display(LinkedList *head)
+{
+    LinkedList *current=head;
+    while(current->next!=head)
+    {
+        
+    }
+}
+
