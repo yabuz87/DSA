34b556064aed0945ad5b56d4e0346c77c354b806
Yabets solomon <jabezsolomonz@gmail.com>
2024-11-20 22:55:35 +0300

    "Added push method to LinkedList struct and modified Stack struct declaration"

--- stack-in-linkedList.cpp
+++ stack-in-linkedList.cpp
@@ -4,6 +4,18 @@ using namespace;
 struct LinkedList{
     LinkedList *next;
     int data;
+    void push(int val)
+    {
+        if(head==nullptr)
+        {
+            head=newNode;
+            
+        }
+        LinkedList newNode=new LinkedList();
+        newNode->data=val;
+        head=newNode;
+
+    }
 };
 
 Struct Stack{
