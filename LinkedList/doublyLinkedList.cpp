f8b6a65675b8fa089075e63fa3780a2ff67a7bfa
Yabets solomon <jabezsolomonz@gmail.com>
2024-11-21 01:48:49 +0300

    the codes are organized  with there stuff   some code are removed since their functionalities are added to other codes

--- LinkedList/doublyLinkedList.cpp
+++ LinkedList/doublyLinkedList.cpp
@@ -5,14 +5,13 @@ struct LinkedList{
     LinkedList *next;
     LinkedList *prev;
 };
- LinkedList *head=nullptr;
 
  void display(LinkedList *head)
  {
     cout<<"[";
     while(head!=nullptr)
     {
-        cout<<head->data<<" ";
+        cout<<head->data<<"<->";
         head=head->next;
     }
     cout<<"]";
@@ -35,28 +34,40 @@ struct LinkedList{
  }
  void insertLast(LinkedList *&head, int val)
  {
-    
+
+
     LinkedList *newNode=new LinkedList();
     newNode->data=val;
     LinkedList *current=head;
-   
+
+if(head==nullptr)
+{
+    head=newNode;
+    head->prev=nullptr;
+}
+else{
     while(current->next!=nullptr)
     {
         current=current->next;
     }
     current->next=newNode;
     newNode->prev=current;
+}
 
  }
 
- 
+
  void insertAt(LinkedList *&head,int val,int position)
  {
-   
+
     LinkedList *newNode=new LinkedList();
     newNode->data=val;
     int pos=1;
-    
+
+    if(head==nullptr)
+    {
+        head=newNode;
+    }
 
  if(position==1)
     {
@@ -66,7 +77,7 @@ struct LinkedList{
         head->prev=nullptr;
 
     }
-   else{ 
+   else{
 
         LinkedList *current=head;
     while(pos<position-1 && current!=nullptr)
@@ -85,8 +96,8 @@ struct LinkedList{
             newNode->prev=current;
             newNode->next=nullptr;
     }
-     
-   
+
+
    else {
     LinkedList *temp=current;
     newNode->next=current->next;
@@ -96,24 +107,314 @@ struct LinkedList{
    }
    }
  }
+void deleteFirst(LinkedList *&head) {
+    if (head == nullptr) {
+        cout << "The list is empty" << endl;
+    } else {
+        LinkedList *temp = head;
+        head = head->next;
+        head->prev = nullptr;
+
+        delete temp;
+    }
+}
+void deleteLast(LinkedList *&head)
+{
+    if(head==nullptr)
+    {
+        cout<<"the list is empty";
+        return;
+    }
+    if(head->next==nullptr)
+    {
+        delete head;
+        cout<<"now the list is empty";
+        head=nullptr;
+    }
+    else
+    {
+        LinkedList *current=head;
+        while(current->next->next!=nullptr)
+        {
+            current=current->next;
+        }
+        LinkedList *temp=current->next;
+        current->next=nullptr;
+        delete temp;
+    }
+}
+void deleteAt(LinkedList *&head, int position) {
+    if (head == nullptr) {
+        cout << "The list is empty" << endl;
+        return;
+    }
+
+    if (position == 1) {
+        LinkedList *temp = head;
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        }
+        delete temp;
+    } else {
+        int pos = 1;
+        LinkedList *current = head;
+        while (pos < position - 1 && current != nullptr) {
+            current = current->next;
+            pos++;
+        }
+
+        if (current == nullptr || current->next == nullptr) {
+            cout << "Position is out of bound" << endl;
+            return;
+        }
+
+        LinkedList *temp = current->next;
+        if (temp->next != nullptr) {
+            temp->next->prev = current;
+        }
+        current->next = temp->next;
+
+        delete temp;
+    }
+}
+void reverse(LinkedList *head) {
+    if (head == nullptr) {
+        cout << "The list is empty" << endl;
+        return;
+    }
+
+    LinkedList *current = head;
+    // Move to the end of the list
+    while (current->next != nullptr) {
+        current = current->next;
+    }
+
+    // Print the list in reverse
+    cout << "[ ";
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->prev;
+    }
+    cout << "]" << endl;
+}
+
+bool search(LinkedList *head,int target)
+{
+    LinkedList *current = head;
+    while (current != nullptr)
+    {
+        int pos=1;
+            if(current->data==target)
+            {
+                // cout<<"Element found at index "<<pos;
+                return true;
+            }
+        current=current->next;
+    }
+    
+    {
+        // cout<<"Element not found\n";
+        return false;
+    } 
+}
+void menu(LinkedList *&head);
+void insertMenu(LinkedList *&head);
+void deleteMenu(LinkedList *&head);
+void insertMenu(LinkedList *&head) {
+    cout << "\t\t\t INSERTION\t\t\t\n\n";
+    cout << "1. Insert At the Beginning\n\n";
+    cout << "2. Insert At the End\n\n";
+    cout << "3. Insert at Any Position\n\n";
+    cout << "0. Back to Menu\n\n";
+
+    int option;
+    int val,Op;
+    int pos;
+    cin >> option;
+
+    switch(option) {
+        case 1:
+            cout << "Enter Value\n";
+            cin >> val;
+            insertFirst(head, val);
+            display(head);
+            cout<<"\n\n0.Exit\n\n1.Back to menu\n\n";
+            cin>>Op;
+            if(Op==1)
+            {
+                system("CLS");
+                insertMenu(head);
+            }
+            else
+            {
+                return;
+            }
+            break;
+        case 2:
+            cout << "Enter Value\n";
+            cin >> val;
+            insertLast(head, val);
+            display(head);
+            cout<<"\n\n0.Exit\n\n1.Back to menu\n\n";
+            cin>>Op;
+            if(Op==1)
+            {
+                system("CLS");
+                insertMenu(head);
+            }
+            else {
+                return;
+            }
+
+            break;
+        case 3:
+            cout << "Enter Value\n";
+            cin >> val;
+            cout << "Enter Position\n";
+            cin >> pos;
+            insertAt(head, pos, val);
+            display(head);
+            cin>>Op;
+            cout<<"\n\n0.Exit\n\n1.Back to menu\n\n";
+
+            if(Op==1)
+            {
+                system("CLS");
+                insertMenu(head);
+            }
+            else
+            {
+                return;
+            }
+            break;
+        case 0:
+            system("CLS");
+            menu(head);
+        default:
+            system("CLS");
+            cout << "Invalid option. Please try again." << endl;
+            insertMenu(head);
+            break;
+    }
+}
+void deleteMenu(LinkedList *&head){
+    cout << "\t\t DELETION\t\t\n\n";
+    cout << "1. Delete At the Beginning\n\n";
+    cout << "2. Delete At the End\n\n";
+    cout << "3. Delete At Any Position\n\n";
+    cout << "0. Back to Menu\n";
+
+    int option, pos,Op;
+    cin >> option;
+
+    switch(option) {
+        case 1:
+            system("CLS");
+            deleteFirst(head);
+            display(head);
+            cout<<"\n\n0.Exit\t\t1.Back to menu\n\n";
+            cin>>Op;
+            if(Op==1)
+            {
+                system("CLS");
+                deleteMenu(head);
+            }
+            break;
+        case 2:
+            system("CLS");
+            deleteLast(head);
+            display(head);
+            cout<<"0.Exit\n\n1.Back to menu\n\n";
+            cin>>Op;
+            if(Op==1)
+            {
+                system("CLS");
+                deleteMenu(head);
+            }
+            break;
+        case 3:
+            cout << "Enter position: ";
+            cin >> pos;
+            system("CLS");
+            deleteAt(head, pos);
+            display(head);
+            cout<<"0.Exit\n\n1.Back to menu\n\n";
+            cin>>Op;
+            if(Op==1)
+            {
+                system("CLS");
+                deleteMenu(head);
+            }
+            break;
+        case 0:
+            system("CLS");
+            menu(head);
+            break;
+        default:
+            cout <<"Invalid option.Please try again." << endl;
+            display(head);
+            deleteMenu(head);
+            cout<<"0.Exit\n\n1.Back to menu\n\n";
+
+            if(Op==1)
+            {
+                system("CLS");
+                deleteMenu(head);
+            }
+    }
+}
+void menu(LinkedList *&head)
+{
+    cout<<"This is a menu for doubly LinkedList\n\n 1.inserting\n\n 2.Delete\n\n 3.Searching\n\n4.Reverse List\n\n";
+    int option,Op;
+    cin>>option;
+    switch(option)
+    {
+         case 1:
+             system("CLS");
+         insertMenu(head);
+         break;
+        case 2:
+           system("CLS");
+         deleteMenu(head);
+         break;
+        case 3:
+
+        cout<<"Enter the number you wanna check\n";
+        int target;
+        cin>>target;
+        system("CLS");
+        cout<<search(head,target);
+        cout<<"0.Exit\n\n1.Back to menu\n\n";
+            cin>>Op;
+            if(Op==1)
+            {
+                system("CLS");
+                deleteMenu(head);
+            }
+
+        break;
+        case 4:
+        system("CLS");
+        reverse(head);
+        cout<<"0.Exit\n\n1.Back to menu\n\n";
+            cin>>Op;
+            if(Op==1)
+            {
+                system("CLS");
+                deleteMenu(head);
+            }
+
+        break;
+
+    }
+}
 
 int main()
 {
-    head=new LinkedList();
-    LinkedList *second=new LinkedList();
-    LinkedList *third=new LinkedList();
-    head->data=40;
-    head->prev=nullptr; 
-    head->next=second; 
-    second->data=50;
-    second->prev=head;
-    second->next=third;
-     third->data=60;
-    third->prev=second;
-    third->next=nullptr;
-     insertFirst(head,30);
-    insertLast(head,89);
-    insertAt(head,44,7);
-    display(head);
+    LinkedList *head;
+    head=nullptr;
+      menu(head);
     return 0;
 }
---
+++ stack/stackLinkedList.cpp
@@ -0,0 +1,213 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    char alpha;
+    Node* next;
+};
+
+struct Stack {
+    Node* top;
+
+    Stack() : top(nullptr) {} // Constructor to initialize top
+
+    void push(int val) {
+        Node* newNode = new Node();
+        newNode->data = val;
+        newNode->next = top;
+        top = newNode;
+    }
+    void push(char val) {
+        Node* newNode = new Node();
+        newNode->alpha = val;
+        newNode->next = top;
+        top = newNode;
+    }
+
+    bool isPalindrome(string str) {
+        int length = str.length();
+
+        for (int i = 0; i < length / 2; i++) {
+            push(str[i]);
+        }
+
+        for (int i = (length + 1) / 2; i < length; i++) {
+            if (str[i] != pop(str)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int pop() {
+        if (top == nullptr) {
+            cout << "Stack Underflow\n";
+            return -1;
+        } else {
+            int data = top->data;
+            Node* temp = top;
+            top = top->next;
+            delete temp;
+            return data;
+        }
+    }
+
+    char pop(string n) {
+        if (top == nullptr) {
+            cout << "Stack Underflow\n";
+            return -1;
+        } else {
+            char data = top->alpha;
+            Node* temp = top;
+            top = top->next;
+            delete temp;
+            return data;
+        }
+    }
+
+    bool isEmpty() {
+        return top == nullptr;
+    }
+
+    void display() {
+        Node* temp = top;
+        if (temp == nullptr) {
+            cout << "Stack is empty\n";
+            return;
+        }
+
+        cout << "|----|\n";
+        while (temp != nullptr) {
+            cout << "| " << temp->data << "  |\n";
+            temp = temp->next;
+        }
+        cout << "|____|\n";
+    }
+
+    void empty() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    void decimalToBinary(int n) {
+        empty();
+        while (n > 0) {
+            push(n % 2);
+            n /= 2;
+        }
+        while (!isEmpty()) {
+            cout << pop();
+        }
+        cout << endl;
+    }
+};
+
+void menu(Stack &s) {
+    cout << "\n\n<<<<<Linked List based Stack>>>>>>\n\n";
+    cout << "1. Push\n\n";
+    cout << "2. Pop\n\n";
+    cout << "3. isEmpty\n\n";
+    cout << "4. Display\n\n";
+    cout << "5. Palindrome Check\n\n6. Decimal to binary\n\n";
+    cout << "Enter your choice: ";
+    int ch;
+    cin >> ch;
+    switch (ch) {
+        int val, Op;
+        case 1: {
+            system("cls");
+            cout << "Enter value: ";
+            cin >> val;
+            s.push(val);
+            s.display();
+            cout << "1. Back to menu     0.Exit\n\n";
+            cin >> Op;
+            if (Op == 1) {
+                system("cls");
+                menu(s);
+            } else
+                break;
+            break;
+        }
+        case 2: {
+            system("cls");
+            cout << "Popped value is " << s.pop() << endl;
+            s.display();
+            cout << "1. Back to menu     0.Exit\n\n";
+            cin >> Op;
+            if (Op == 1) {
+                system("cls");
+                menu(s);
+            } else
+                break;
+            break;
+        }
+        case 3: {
+            system("cls");
+            cout << (s.isEmpty() ? "Stack is empty" : "Stack is not empty") << endl;
+            cout << "1. Back to menu     0.Exit\n\n";
+            cin >> Op;
+            if (Op == 1) {
+                system("cls");
+                menu(s);
+            } else
+                break;
+            break;
+        }
+        case 4: {
+            system("cls");
+            s.display();
+            cout << "1. Back to menu     0.Exit\n\n";
+            cin >> Op;
+            if (Op == 1) {
+                system("cls");
+                menu(s);
+            } else
+                break;
+            break;
+        }
+        case 5: {
+            system("cls");
+            cout << "Enter the String: \n\n";
+            string Str = " ";
+            cin >> Str;
+            cout << "\n\n" << "is \"" << Str << "\" palindrome ? " << (s.isPalindrome(Str) ? "Yes" : "No") << endl;
+            cout << "1. Back to menu     0.Exit\n\n";
+            cin >> Op;
+            if (Op == 1) {
+                system("cls");
+                menu(s);
+            } else
+                return;
+            break;
+        }
+        case 6: {
+            system("cls");
+            cout << "Enter the number\n\n";
+            cin >> val;
+            s.decimalToBinary(val);
+            s.display();
+            cout << "1. Back to menu     0.Exit\n\n";
+            cin >> Op;
+            if (Op == 1) {
+                system("cls");
+                menu(s);
+            } else
+                break;
+            break;
+        }
+        default: {
+            cout << "Invalid choice\n";
+            menu(s);
+            break;
+        }
+    }
+}
+
+int main() {
+    Stack s;
+    menu(s);
+    return 0;
+}
