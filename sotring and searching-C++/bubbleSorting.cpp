798a445cb0eca8b8b78e31679dde0b64c212037c
Yabets solomon <jabezsolomonz@gmail.com>
2024-11-25 15:24:15 +0300

    Added "algorithm" to files.associations in settings.json

---
+++ sotring and searching-C++/bubbleSorting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void bubbleSort(int num[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(num[j]>num[j+1])
+            {
+                int temp=num[j];
+                num[j]=num[j+1];
+                num[j+1]=temp;
+            }
+        }
+    }
+}
+void display(int num[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<num[i]<<" ";
+    }
+}
+int main()
+{
+    int num[]={1,4,8,-2,5};
+    bubbleSort(num,5);
+    display(num,5);
+}
