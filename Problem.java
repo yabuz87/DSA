fb868331fdf3bd186a45915e9f773c7b719e2a10
Yabets solomon <jabezsolomonz@gmail.com>
2024-12-22 01:20:24 +0300

    added some java code in DSA

--- Problem.java
+++ Problem.java
@@ -1,35 +1,58 @@
 public class Problem{
     public static void main(String[] args)
     {
-        System.out.println(new Problem().checkLength("aabbaa", 3));
+        System.out.println(new Problem().checkLength("aaaabaab", 4));
+        System.out.println(new Problem().numberOfSub("zzzz"));
     }
 
 
+
     int checkLength(String s, int k) {
-        int n = s.length();
+        int n = s.length()-1;
         int left = 0;
-        int right = k;
-        int maxLength = 0;
+        int right = k-1;
+        int maxLength=k;
        
     
         while (right < n) {
             int tempLength = 0;
-            boolean checked = true;
+            boolean checked = false;
             for (int i = left; i < right; i++) {
-                if (s.charAt(i) != s.charAt(i + 1) && !checked) {
+                if (s.charAt(i) != s.charAt(i + 1) && checked==false) {
                     tempLength++;
                     checked = true;
-                } else if (s.charAt(i) != s.charAt(i + 1) && checked) {
+                } else if (s.charAt(i) != s.charAt(i + 1) && checked==true) {
                     checked = false;
                 }
             }
             
-            maxLength = Math.max(tempLength, maxLength);
+            maxLength = Math.min(tempLength, maxLength);
             left++;
             right++;
         }
         
         return maxLength;
+    }
+    int numberOfSub(String a){
+        char[] arr = a.toCharArray();
+        int count=0;
+        if(arr.length==0)
+        {
+            return 1;
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+                for(int j=i;j<arr.length;j++)
+                {
+                    if(arr[i]==arr[j])
+                    {
+                        count++;
+
+                    }
+                }
+
+        }
+        return count;
     }
     
 }
