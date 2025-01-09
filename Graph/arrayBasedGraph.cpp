2f5bc223a754e6568f42e1499320e83e8c0a1ed3
Yabets solomon <jabezsolomonz@gmail.com>
2025-01-09 16:29:12 +0300

    `Added adjacency matrix initialization and updated main function to demonstrate graph usage.`

--- Graph/arrayBasedGraph.cpp
+++ Graph/arrayBasedGraph.cpp
@@ -13,6 +13,7 @@ struct Graph {
         adjMatrix = new int*[v];
         for(int i = 0; i < v; ++i) {
             adjMatrix[i] = new int[v];
+           
             for(int j = 0; j < v; ++j) {
                 adjMatrix[i][j] = 0;
             }
@@ -40,16 +41,22 @@ struct Graph {
 };
 
 int main() {
-    Graph n(10);
-    n.addEdge(0, 1); 
+    Graph n(5);
+    n.addEdge(0, 1);
+    n.addEdge(0, 2);
+    n.addEdge(4,2); 
 
     // Display the adjacency matrix
+    cout<<"this one is the adjacency matrix of the graph\n";
+    cout<<"  _ _ _ _ _ _ \n";
     for (int i = 0; i < n.v; ++i) {
+         cout<<"\n| ";
         for (int j = 0; j < n.v; ++j) {
             cout << n.adjMatrix[i][j] << " ";
         }
-        cout << "\n";
+        cout<<" |\n";
     }
+   cout<<"  _ _ _ _ _ _ \n";
 
     return 0;
 }
