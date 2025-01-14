48355031dba45ae0690f98d9b4281e2d6c0f94fc
Yabets solomon <jabezsolomonz@gmail.com>
2025-01-14 04:57:05 +0300

    `Refactor DeleteBST function in tree deletion implementations`

--- tree/DeleteByCopying.cpp
+++ tree/DeleteByCopying.cpp
@@ -1,3 +1,50 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct node{
+    int data;
+    node *right;
+    node *left;
+};
+
+node* root = nullptr;
+
+void insert(int val) {
+    node* newNode = new node();
+    newNode->data = val;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+
+    if (root==nullptr)
+    {
+        root=newNode;
+        cout<<val<<" has added successfully\n";
+        return;
+    }
+
+    node *temp=root;
+    node *parent = nullptr;
+
+    while (temp != nullptr) {
+        parent = temp;
+        if (val < temp->data) {
+            temp = temp->left;
+        } else {
+            temp = temp->right;
+        }
+    }
+
+    if (val < parent->data) {
+        parent->left = newNode;
+        cout<<val<<" has added successfully\n";
+        return;
+    } else {
+        parent->right = newNode;
+        cout<<val<<" has added successfully\n";
+    }
+}
+
 void DeleteBST(Node *RNP, Node *PDNP, int x)
 {
     Node *DNP; // a pointer that points to the currently deleted node
--- tree/deleteByMerge.cpp
+++ tree/deleteByMerge.cpp
@@ -1,29 +1,101 @@
-void DeleteBST(Node* &RNP, int x) {
-    if (RNP == NULL) {
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct node{
+    int data;
+    node *right;
+    node *left;
+};
+
+node* root = nullptr;
+
+void insert(int val) {
+    node* newnode = new node();
+    newnode->data = val;
+    newnode->left = nullptr;
+    newnode->right = nullptr;
+
+    if (root==nullptr)
+    {
+        root=newnode;
+        cout<<val<<" has added successfully\n";
+        return;
+    }
+
+    node *temp=root;
+    node *parent = nullptr;
+
+    while (temp != nullptr) {
+        parent = temp;
+        if (val < temp->data) {
+            temp = temp->left;
+        } else {
+            temp = temp->right;
+        }
+    }
+
+    if (val < parent->data) {
+        parent->left = newnode;
+        cout<<val<<" has added successfully\n";
+        return;
+    } else {
+        parent->right = newnode;
+        cout<<val<<" has added successfully\n";
+    }
+}
+
+void DeleteBST(node* &RNP, int x) {
+    if (RNP == nullptr) {
         cout << "Data not found\n";
         return;
     }
 
-    if (x < RNP->Num) {
-        DeleteBST(RNP->Left, x);  // delete the element in the left subtree
-    } else if (x > RNP->Num) {
-        DeleteBST(RNP->Right, x); // delete the element in the right subtree
+    if (x < RNP->data) {
+        DeleteBST(RNP->left, x);  // delete the element in the left subtree
+    } else if (x > RNP->data) {
+        DeleteBST(RNP->right, x); // delete the element in the right subtree
     } else {
-        Node* DNP = RNP;
-        if (RNP->Left == NULL) {
-            RNP = RNP->Right; // replace RNP with its right child
-        } else if (RNP->Right == NULL) {
-            RNP = RNP->Left;  // replace RNP with its left child
+        node* DNP = RNP;
+        if (RNP->left == nullptr) {
+            RNP = RNP->right; // replace RNP with its right child
+        } else if (RNP->right == nullptr) {
+            RNP = RNP->left;  // replace RNP with its left child
         } else {
             // Find the minimum node in the right subtree
-            Node* MinNode = RNP->Right;
-            while (MinNode->Left != NULL) {
-                MinNode = MinNode->Left;
+            node* Minnode = RNP->right;
+            while (Minnode->left != nullptr) {
+                Minnode = Minnode->left;
             }
             // Merge the right subtree with the left subtree
-            MinNode->Left = RNP->Left;
-            RNP = RNP->Right; // replace RNP with its right child
+            Minnode->left = RNP->left;
+            RNP = RNP->right; // replace RNP with its right child
         }
         delete DNP; // delete the node
     }
+}
+void preOrderTraversal(node* root)
+{
+    if(root==nullptr)
+    return;
+    cout<<root->data<<" ";
+    preOrderTraversal(root->left);
+    
+    preOrderTraversal(root->right);
+}
+int main()
+{
+    insert(10);
+    insert(12);
+    insert(14);
+    insert(9);
+    insert(6);
+    insert(0);
+    preOrderTraversal(root);
+    cout<<"\n";
+    DeleteBST(root,9);
+    preOrderTraversal(root);
+
+
+    
 }
